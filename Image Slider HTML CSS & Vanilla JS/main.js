c130b28333d6cc07e6ed315d797a408fe9450fd3
nazariaskar7 <85358924+nazariaskar7@users.noreply.github.com>
2022-02-04 02:46:53 +0430

    Add files via upload

---
+++ Image Slider HTML CSS & Vanilla JS/main.js
@@ -0,0 +1,37 @@
+const slides = document.querySelectorAll('.slide');
+const next = document.querySelector('#next');
+const prev = document.querySelector('#prev');
+
+
+const nextSlide = () => {
+    const current = document.querySelector('.current');
+    current.classList.remove('current');
+    if(current.nextElementSibling){
+        current.nextElementSibling.classList.add('current');
+    }else {
+        slides[0].classList.add('current');
+    }
+}
+
+const prevSlide = () => {
+    const current = document.querySelector('.current');
+    current.classList.remove('current');
+    if(current.previousElementSibling){
+        current.previousElementSibling.classList.add('current');
+    }else {
+        slides[slides.length - 1].classList.add('current');
+    }
+}
+
+next.addEventListener('click', e => {
+    nextSlide();
+})
+
+prev.addEventListener('click', e => {
+    prevSlide();
+})
+
+if(false){
+    let slideInterval = setInterval(nextSlide, 4000);
+}
+
